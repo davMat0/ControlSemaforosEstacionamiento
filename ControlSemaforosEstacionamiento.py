31df8ab1aada618bef0ffd6ed9c245d9dba5a3f8
dmate <david.matalo.1@educa.jcyl.es>
2024-11-25 17:13:40 +0100

    Initial commit

---
+++ ControlSemaforosEstacionamiento.py
@@ -0,0 +1,24 @@
+import threading
+import time
+import random
+
+semaforo = threading.Semaphore(3)
+
+def accederEstacionamiento(idVehiculo):
+    semaforo.acquire()
+    print(f"Vehículo {idVehiculo} ha entrado al estacionamiento")
+    time.sleep(random.uniform(5,10))
+    print(f"Vehículo {idVehiculo} ha salido del estacionamiento")
+    semaforo.release()
+
+vehiculos = 10
+
+hilos = []
+for i in range(10):
+    hilo = threading.Thread(target=accederEstacionamiento, args=(i,))
+    hilo.start()
+    hilos.append(hilo)
+
+for hilo in hilos:
+    hilo.join()
+
